6b20e272ee0a81c9b225cc08be178325181f7979
Qasim Soomro <qasim@skynur.com>
2022-10-13 19:33:40 +0000

    support mobile responsive

---
+++ _next/static/BQ1C2YvKKZe-innoJlPvT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/428-65818b76926f00c3.js","static/chunks/pages/index-563fe5bc40694af3.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
