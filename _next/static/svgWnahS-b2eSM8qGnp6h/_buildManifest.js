d46f8cccc4d48f6765cde36d5be69cf807faf6eb
Qasim Soomro <qasim@skynur.com>
2022-10-14 23:09:21 +0000

    support export

--- _next/static/svgWnahS-b2eSM8qGnp6h/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/478-4d6b4b7405a2e268.js","static/chunks/pages/index-fdc8af9d8e77d975.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
