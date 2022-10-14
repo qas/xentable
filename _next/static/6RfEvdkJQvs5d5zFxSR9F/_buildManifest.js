2c0a8aedaf7a36fd276f17448c2136b528df9737
Qasim Soomro <qasim@skynur.com>
2022-10-14 19:47:35 +0000

    support all text types

---
+++ _next/static/6RfEvdkJQvs5d5zFxSR9F/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/478-4d6b4b7405a2e268.js","static/chunks/pages/index-22b7d0c9702e54a4.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
