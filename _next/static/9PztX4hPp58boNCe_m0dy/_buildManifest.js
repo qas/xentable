7b177d435a0e6fd3e518a7b6accb5a784f55f4d9
Qasim Soomro <qasim@skynur.com>
2022-10-15 04:50:58 +0000

    add info label

---
+++ _next/static/9PztX4hPp58boNCe_m0dy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/741-bb0209273987d7de.js","static/chunks/pages/index-5bc1605eeed8512a.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
