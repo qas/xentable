deab26a85ec1526efee062eb12867168f043516b
Qasim Soomro <qasim@skynur.com>
2022-10-19 00:37:36 +0000

    support multiple networks

--- _next/static/RBgr_0nCHpt6WZ7VrlqJN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/365-b8890131d2abe8d2.js","static/chunks/pages/index-0458cb15923019f9.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
